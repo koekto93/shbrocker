a03236ba8d798abb2a24904f1d2c78c48020535e
Mark Ulybkin <Mark.Ulybkin@leroymerlin.ru>
2019-08-02 15:09:58 +0300

    material

--- src/components/pages/MainPage/index.js
+++ src/components/pages/MainPage/index.js
@@ -1,13 +1,53 @@
-import React, { Component } from 'react';
+import React from 'react';
+//import { makeStyles } from '@material-ui/core/styles';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
 
-export class MainPage extends Component {
-	render() {
-		return (
-			<div>
-				<div>Главная страница</div>
-			</div>
-		);
+/* const useStyles = makeStyles(theme => ({
+	root: {
+		display: 'flex',
+		flexWrap: 'wrap',
+	},
+	formControl: {
+		margin: theme.spacing(1),
+		minWidth: 120,
+	},
+	selectEmpty: {
+		marginTop: theme.spacing(2),
+	},
+})); */
+
+const MainPage = () => {
+	//const classes = useStyles();
+	const [values, setValues] = React.useState({
+		age: '',
+		name: 'hai',
+	});
+
+	function handleChange(event) {
+		setValues(oldValues => ({
+			...oldValues,
+			[event.target.name]: event.target.value,
+		}));
 	}
-}
+
+	return (
+		<div>
+			<div>Главная страница</div>
+			<Select
+				value={values.age}
+				onChange={handleChange}
+				inputProps={{
+					name: 'age',
+					id: 'age-simple',
+				}}
+			>
+				<MenuItem value={10}>Ten</MenuItem>
+				<MenuItem value={20}>Twenty</MenuItem>
+				<MenuItem value={30}>Thirty</MenuItem>
+			</Select>
+		</div>
+	);
+};
 
 export default MainPage;
